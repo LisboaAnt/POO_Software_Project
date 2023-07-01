54e5ec3e64417346ec61f08af5d5547574dcddb9
Antonio Lisboa de Carvalho <antoniol.carvalho49@gmail.com>
2023-07-01 20:07:59 -0300

    main update

--- Code/src/SubmissaoDeArtigos/main.java
+++ Code/src/SubmissaoDeArtigos/main.java
@@ -12,11 +12,11 @@ public class main { //poe o derby na libraries na classpath
 
 
     public static void main(String[] args) {
-        /*
+        
         Model model = new Model(); // Primeiro instancia-se o modelo
         MainView view = new MainView(); // Depois cria-se a primeira view
         view.initMainView(model); // Por fim, inicializa a view passando o modelo
-        */
+        
             
         DAO dao = DAO.getInstance();
         //dao.DatabaseInitializer();
