d78040de2e3ff7dc39225dc1bafcc7f241ff1dca
Antonio Lisboa de Carvalho <antoniol.carvalho49@gmail.com>
2023-07-02 23:36:02 -0300

    .

--- Code/src/SubmissaoDeArtigos/main.java
+++ Code/src/SubmissaoDeArtigos/main.java
@@ -16,31 +16,17 @@ public class main { //poe o derby na libraries na classpath
         
         Model model = new Model(); // Primeiro instancia-se o modelo
         MainView view = new MainView(); // Depois cria-se a primeira view
-        view.initMainView(model);
+        view.initMainView(model); //passa a model para criar sua controladora
         
 
             
         DAO dao = DAO.getInstance();
-        Revisor r = new Revisor("Nome","mail","enha","especialidade");
-        dao.databaseInset(r);
-        //dao.DatabaseInitializer();
+        //Revisor r = new Revisor("Nome","email","senha","especialidade");
+        //dao.databaseInset(r); //insere o revisor
+        //dao.DatabaseInitializer(); //Cria a pasta do banco
 
-        //Pessoa a = new Pessoa("Luis","lis.gmail", "1235", "Autor");
-        //dao.databaseInsert(a);
-        
-        
-        /* BUSCAR LOGIN
-        DatabaseGetter.getDataFromDatabase();
-        Pessoa aa = dao.verificarLogin("lis.gmail", "1235");
-        if(aa != null){System.out.println(aa.toString());
-        }
-        */
-       
-        //RevisorView viewRevisor = new RevisorView();
-        //viewRevisor.initRevisorView(model);
-        // Configura o ProgramaFechador como o listener para o evento de fechamento da janela principal
-
-
+        //Pessoa a = new Pessoa("Luis","lis.gmail", "1235", "Autor"); //cria pessoa
+        //dao.databaseInsert(a);//insere pessoa
              
     }
 }
